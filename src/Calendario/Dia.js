ef2209b35d9f947723211c3567825359e0ec78a5
dj <dj@ime.usp.br>
2021-11-16 17:56:33 -0300

    Adicionando react-calendar

--- src/Calendario/Dia.js
+++ src/Calendario/Dia.js
@@ -1,9 +1,37 @@
-function Dia({data,mes,ano}) {
-    return (
-        <button>
-            { (data > 0 && data < 32) ? data : '-' }
-        </button>
-    )
+import React from 'react';
+
+class Dia extends React.Component {
+
+    state = { qtdCliques: '0' }
+    
+    constructor () {
+        super();
+        this.handleOnClick = this.handleOnClick.bind(this);
+    }
+
+    handleOnClick(event) {
+        this.setState( {  qtdCliques: 1 } );
+        console.log("Clicou " + this.state.qtdCliques + " vezes em " + this.props.data)
+    }
+
+    render () {
+        return (
+            <button onClick={this.handleOnClick}>
+                { 
+                    (this.props.data > 0 && this.props.data < 32) ? 
+                        this.props.data : 
+                        '-' 
+                }
+            </button>
+        )
+    }
 }
 
+// function Dia({data,mes,ano}) {
+//     return (
+//         <button>
+//             { (data > 0 && data < 32) ? data : '-' }
+//         </button>
+//     )
+// }
 export default Dia;
--- src/PaginaUM.js
+++ src/PaginaUM.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from './components/Header'
 import Calendario from './Calendario/Calendario';
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
 
 function PaginaUM() {
     return (
         <div>
             <Header fontSize='30' titulo="Pagina UM"/>
+            <p> Nosso Calendario: </p>
             <Calendario mes='11' ano='2021'/>
+            <p> Calendario do React: </p>
+            <Calendar calendarType="US"/>
         </div>
     )
 }
